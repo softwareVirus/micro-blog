e60983a6170c684f6427f7b1217dbd0f27551004
Ibrahim Sakli <19070001004@stu.yasar.edu.tr>
2024-01-24 08:48:44 +0300

    feat(err): add error display page

---
+++ client/src/components/SfErrorPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 style="margin: 20rem; font-size: 8rem;">There is no page!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped></style>
