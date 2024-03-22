0e361731adca34d0a713061986e469b6d8c99ada
Ibrahim Halil Sakli <ibrahimsdakli52@hotmail.com>
2024-03-22 11:24:57 +0300

    feat: Add tag and follow fields to user model

--- server/app/models/user.py
+++ server/app/models/user.py
@@ -1,5 +1,13 @@
-from mongoengine import Document, StringField, EmailField, DateTimeField
+from mongoengine import (
+    Document,
+    StringField,
+    EmailField,
+    DateTimeField,
+    ListField,
+    ReferenceField,
+)
 import datetime
+
 
 class User(Document):
     """
@@ -15,6 +23,12 @@ class User(Document):
         Email of the user, unique and required.
     hashed_password : StringField
         Hashed password of the user, required.
+    tags : ListField
+        List of references to Tag documents associated with the user.
+    followers : ListField
+        List of references to other User documents who follow this user.
+    following : ListField
+        List of references to other User documents whom this user follows.
     created_at : DateTimeField
         Timestamp representing the user's creation date (default: current UTC time).
     updated_at : DateTimeField
@@ -42,6 +56,9 @@ class User(Document):
     last_name = StringField(max_length=200, required=True)
     email = EmailField(unique=True, required=True)
     hashed_password = StringField(required=True)
+    tags = ListField(ReferenceField("Tag"))
+    followers = ListField(ReferenceField("User"))
+    following = ListField(ReferenceField("User"))
     created_at = DateTimeField(default=datetime.datetime.utcnow())
     updated_at = DateTimeField(default=datetime.datetime.utcnow())
 
@@ -59,6 +76,9 @@ class User(Document):
             "first_name": self.first_name,
             "last_name": self.last_name,
             "email": self.email,
+            "tags": [str(tag.id) for tag in self.tags],
+            "followers": [str(follower.id) for follower in self.followers],
+            "following": [str(following.id) for following in self.following],
             "created_at": self.created_at.isoformat(),
             "updated_at": self.updated_at.isoformat(),
         }
