302f381d08c5a2a3ab8fae4036f09e8b0985d62b
Ibrahim Halil Sakli <ibrahimsdakli52@hotmail.com>
2024-04-04 20:20:18 +0300

    feat: Add chat history and message dialog routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -7,6 +7,8 @@ import SfCreatePost from "../components/Post/CreatePost/SfCreatePost";
 import SfHomePage from "../components/SfHomePage";
 import SfErrorPage from "../components/SfErrorPage";
 import SfUserProfile from "../components/UserProfile/SfUserProfile"
+import SfMessageDialog from '../components/Chat/SfMessageDialog.vue';
+import SfChatHistory from "../components/Chat/SfChatHistory.vue"
 import { store } from "../store/index";
 
 const routes = [
@@ -19,6 +21,11 @@ const routes = [
     path: "/signup",
     component: SfSignup,
     meta: { requiresAuth: false },
+  },
+  {
+    path: "/chat_history",
+    component: SfChatHistory,
+    meta: { requiresAuth: true },
   },
   {
     path: "/profile/:id",
@@ -39,6 +46,11 @@ const routes = [
     path: "/create-post",
     component: SfCreatePost,
     meta: { requiresAuth: true },
+  },
+  {
+    path: '/chat/:cid/:rid',
+    component: SfMessageDialog,
+    props: true // Pass route params as props to the component
   },
   {
     path: "/",
